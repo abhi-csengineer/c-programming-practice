87852a45ece678dd8d1a7c7fc3348dff7636fdc5
Abhinav sah <abhiitiansah@gmail.com>
2025-12-27 12:46:08 +0530

    Update and rename Arithmetic,+-*div to Arithmetic.c

---
+++ Arithmetic.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (void)
+{
+int a,b;
+printf("Enter the first no a:\n");
+printf("Enter the second no b:\n");
+scanf("%d %d",&a,&b);
+printf("=The answer is %d" a+b); //Here we can sub,multiple,div by replacing the addition sign with them.
+return 0;
+}
