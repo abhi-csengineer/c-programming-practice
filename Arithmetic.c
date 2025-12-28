f6467b34763e44ce9e1d95cca6b0551d8c5eac37
Abhinav sah <abhiitiansah@gmail.com>
2025-12-28 13:30:37 +0530

    Fix printf syntax for displaying addition result

--- Arithmetic.c
+++ Arithmetic.c
@@ -5,6 +5,6 @@ int a,b;
 printf("Enter the first no a:\n");
 printf("Enter the second no b:\n");
 scanf("%d %d",&a,&b);
-printf("=The answer is %d" a+b); //Here we can sub,multiple,div by replacing the addition sign with them.
+printf("=The answer is %d",a+b); //Here we can sub,multiple,div by replacing the addition sign with them.
 return 0;
 }
